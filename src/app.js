933b6e228748422b659eeabe315198e4148cf33e
Prabudh Mishra <prabudh.mishra18@gmail.com>
2025-10-01 22:33:43 +0200

    Setup Logging and Middlewares for project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import logger from "#config/logger.js"
 
 const app = express();
 
+app.use(helmet());
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('combined', {stream: {write: (message) => logger.info(message.trim())}}));
+
 app.get('/', (req, res) => {
+    logger.info("Hello from acquisitions")
   res.status(200).send('Hello from Acquisitions API');
 });
 
